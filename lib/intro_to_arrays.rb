d4232ac1b406d71c34fcc1ac0b85d7c67c0a81ef
Peter Meister <prmeister89@gmail.com>
2018-07-05 20:31:26 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["element1", "element2"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array(array)
+  array.
